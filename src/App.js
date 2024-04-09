989464b4a11eaa593fbdc88bd4d9ceb35608a4d1
pawan <thakunipawan@gmail.com>
2024-04-09 22:04:04 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import {useEffect, useState} from 'react'
+
+function App() {
+  const[search,setSearch]=useState([]);
+
+  const[searchText,setSearchText]=useState("");
+ 
+
+ const handleClick=(e)=>{
+  console.log(e);
+  
+  if(searchText.trim() !==" "){
+      setSearch(searchText)
+      setSearchText('')
+    
+  }
+}
+  
+  
+  
+
+  
+  return (
+    <div className="App">
+        <input type='text' value={searchText} onChange={(e)=>setSearchText(e.target.value)}></input>
+        <button onClick={()=>handleClick}></button>
+
+        {
+search.map(()=>{
+  return(
+    <h1>{search}</h1>
+  )
+})
+        }
+
+         
+    </div>
+  );
+}
+
+export default App;
